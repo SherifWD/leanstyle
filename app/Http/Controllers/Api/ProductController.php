a5ef07e974ba1677d53f56092824f9dc2bb2cb89
SherifWD <sherifshahawywd@gmail.com>
2025-09-03 01:13:20 +0300

    ed

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -83,7 +83,7 @@ class ProductController extends Controller
     // GET /api/store/{store}/products?view=list|grid&sort=&page=
     public function storeProducts(Store $store, Request $request)
     {
-        $q = $store->products()->where('is_active', true)->with(['images','category','brand','variants','views']);
+        $q = $store->products()->with(['images','category','brand','variants.size','variants.color','views'])->where('is_active', true);
 
         match ($request->get('sort')) {
             'price_asc'  => $q->orderBy('price', 'asc'),
