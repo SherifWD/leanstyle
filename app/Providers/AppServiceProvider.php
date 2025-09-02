1033ef38dcb68c2c3f1620b4c5911245fa184e19
SherifWD <sherifshahawywd@gmail.com>
2025-09-03 01:54:20 +0300

    ed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Log::withContext([
+        'trace_id' => app()->bound('request_id') ? app('request_id') : null,
+    ]);
     }
 }
